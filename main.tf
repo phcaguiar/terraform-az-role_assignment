ee8d7eb2d41135513076ab0064f89d0d31ba6839
Pedro Aguiar <pedro_deaguiar@yahoo.com.br>
2018-12-03 14:39:21 +0000

    Adding terraform data resource for resource_group

--- main.tf
+++ main.tf
@@ -1,5 +1,9 @@
+data "azurerm_resource_group" "resource_group" {
+  name = "${var.azure_resource_group}"
+}
+
 resource "azurerm_role_assignment" "role_assignment" {
-  scope                 = "${var.azure_resource_group}"
-  role_definition_name  = "${var.role_definition_name}"
-  principal_id          = "${var.role_assignment_principal_id}"
+  scope                 = "${data.azurerm_resource_group.resource_group.id}"
+  role_definition_name  = "${var.azure_role_definition_name}"
+  principal_id          = "${var.azure_role_assignment_principal_id}"
 }
